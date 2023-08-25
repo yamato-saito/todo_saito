6d57e4c25c3adf3ab1708fe06daa802b929983f4
yamato-saito <yamato970428@docomo.ne.jp>
2023-08-25 14:15:32 +0900

    ログアウトまで

--- todo-app/todoapp/routes/index.js
+++ todo-app/todoapp/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 const mysql = require('mysql');
 
 
@@ -12,39 +13,53 @@ const connection = mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      console.log(error);
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+
+  console.log(`isAuth: ${isAuth}`);
+  
+  
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
       console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
-    }
-  );
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
+      });
+    });
 });
 
 /*post*/
-router.post('/', function(req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
+router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
 
   const todo = req.body.add;
-
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+  knex("tasks")
+    .insert({user_id: userId, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
+      });
+    });
 });
 
+router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
